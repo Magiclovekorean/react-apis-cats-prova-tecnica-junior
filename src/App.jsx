9e26445f457d7ed7ef74b0615569e9d203d1391a
Martí Forn <magiclovett@gmail.com>
2026-02-04 14:52:25 +0100

    Added 2nd api in use
    For now, it only renders an image of a cat with the text "hello"

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,34 @@
 import { useState, useEffect } from 'react'
 import './App.css'
+const CAT_RANDOM_FACT_ENDPOINT = "https://catfact.ninja/fact"
+const CAT_IMAGE_URL_ENDPOINT = "https://cataas.com/cat/says/"
+
 function App() {
 
 
     const [catFact, setCatFact] = useState()
 
+    const [catImageUrl, setCatImageUrl] = useState()
+
     useEffect(() => {
-        fetch("https://catfact.ninja/fact")
+        fetch(CAT_RANDOM_FACT_ENDPOINT)
             .then(response => response.json())
             .then(data => setCatFact(data.fact))
     }, [])
 
+
+    useEffect(() => {
+        fetch("https://cataas.com/cat/says/hello?json=true")
+            .then(response => response.json())
+            .then(data => setCatImageUrl(data.url))
+        console.log(`hello, the catFact state is ${catImageUrl}`)
+    }, [catFact])
+
     return (
         <main>
-            <h1>App de gatitos</h1>
+            <h1>App de gatos</h1>
             {catFact && <p>{catFact}</p>}
+            {catImageUrl && <img src={catImageUrl} />}
         </main>
     )
 }
